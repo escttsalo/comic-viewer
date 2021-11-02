fa8a3dc786a08f1a027364ab9aa7701f7da97364
escttsalo <escttsalo@gmail.com>
2021-11-02 03:20:19 -0600

    move "app" into index, import font awesome, play with logos/icons in navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Box, Icon, Navbar } from 'react-bulma-components';
+
+// const home = () => {
+//     return (
+//         <Box>
+//             {/* <Icon renderAs={Link} color="primary" to="/" style={{ border: '1px solid red'}}> */}
+//             <Icon style={{ color:'turquoise'}}>
+//                 <i className="fas fa-home fa-2x" />
+//             </Icon>
+//         </Box>
+//     );
+// };
+
+const logo = () => {
+    return (
+        <Box flexDirection={'column'} justifyContent={'space-around'} style={{ color: 'turquoise'}}>
+            <Icon style={{ color: 'turquoise'}}>
+                <i className="fab fa-github fa-3x"/>
+            </Icon>
+        </Box>
+    );
+};
+
+// const menu = () => {
+//     return ( 
+//         <Box>
+            
+//         </Box>
+//     );
+// };
 
 export default function Navigation(){
     return (
-        <nav>
-            <span>Home</span>
-            <span>Logo</span>
+        <Navbar color={'dark'}>
+            <Navbar.Brand renderAs={'div'} href="#">
+                {/* <Navbar.Link active={false}> */}
+                    { logo() }
+                {/* </Navbar.Link> */}
+            </Navbar.Brand>
             <span>Menu</span>
-        </nav>
-    )
-}
+        </Navbar>
+
+        // <nav>
+        //     {
+        //         home()
+        //     }
+        //     <span>Logo</span>
+        //     <span>Menu</span>
+        // </nav>
+    );
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'bulma/css/bulma.min.css';
+
+//components
+import Navigation from './components/Navigation';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -9,3 +12,22 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+//I'll explain later promise
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Navigation />
+      </header>
+      <section>
+        <div>
+          Home page
+        </div>
+      </section>
+      <footer>
+        Check
+      </footer>
+    </div>
+  );
+}
