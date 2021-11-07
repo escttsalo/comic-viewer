e693a7097f07985ce1887a7e33c0f636d3070642
escttsalo <escttsalo@gmail.com>
2021-11-07 07:14:35 -0700

    creator dashboard link with route

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Block, Form, Heading, Box, Section, Notification, Columns, Icon } from 'react-bulma-components'; //eslint-disable-line
 const { Column } = Columns;
 const { Control, Field, Select } = Form;
@@ -15,11 +16,12 @@ export default function Profile() {
 
     return (
         <div>
-            <Section backgroundColor='black' style={{height: '75vh'}}>
-                <Columns backgroundColor='dark'>
+            <Section backgroundColor='black' >
+                <Columns backgroundColor='dark' marginless style={{paddingBottom: '3rem'}}>
                     {/* Profile*/}
-                    <Column size={'one-third'} offset={1}>
+                    <Column size={'one-third'} offset={1} style={{marginTop: '3%'}}>
                         <Box backgroundColor='black'>
+                            {/* Profile Information */}
                             <Notification backgroundColor='dark' color='black' textAlign='center'>
                                 <Heading textFamily='code'>
                                     Profile Information
@@ -27,6 +29,7 @@ export default function Profile() {
                             </Notification>
                             <Box backgroundColor='dark'>
                                 <Block backgroundColor='dark' textColor='light'>
+                                    {/* Username */}
                                     <Heading subtitle textColor='light' style={{marginBottom: 10, marginTop: 5}}>
                                         <i class="fas fa-user"/> Username
                                     </Heading>
@@ -36,6 +39,7 @@ export default function Profile() {
                                         }
                                     </p>
 
+                                    {/* Email */}
                                     <Heading subtitle textColor='light' style={{marginBottom: 10}}>
                                         <i class="fas fa-solid fa-at"/> Email
                                     </Heading>
@@ -45,11 +49,12 @@ export default function Profile() {
                                         }
                                     </p>
 
+                                    {/* Account Type */}
                                     <Heading size={4} textColor='light' style={{marginBottom: 10, marginLeft: '5%'}}>
                                         Account Type
                                     </Heading>
                                     <Notification backgroundColor='dark' paddingless style={{marginLeft: '5%'}}>
-                                        Here you can change your user role to that of a creator. Unlocking the creator dashboard.
+                                        Here you can change your role to that of a creator.<br/> Unlocking the creator dashboard.
                                     </Notification>
 
                                     <Field style={{marginLeft: '5%'}} backgroundColor='dark'>
@@ -70,13 +75,24 @@ export default function Profile() {
                                             </Select>
                                         </Control>
                                     </Field>
+
+                                    <Notification 
+                                        to={'/creator'} 
+                                        renderAs={Link} 
+                                        backgroundColor='dark' 
+                                        textColor='link'
+                                        paddingless 
+                                        style={{marginLeft: '5%'}}
+                                    >
+                                        Creator Dashboard
+                                    </Notification>
                                 </Block>
                             </Box>
                         </Box>
                     </Column>
 
                     {/* Favourites */}
-                    <Column size={'one-third'} offset={2}>
+                    <Column size={'one-third'} offset={2} style={{marginTop: '3%'}}>
                         <Box backgroundColor='black'>
                             <Notification backgroundColor='dark' color='black' textAlign='center'>
                                 <Heading textFamily='code'>
