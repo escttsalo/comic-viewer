c5ec6205cd033dc4db47c9698c839ad7dddfe683
escttsalo <escttsalo@gmail.com>
2021-11-07 06:54:46 -0700

    dropdown for account type

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Block, Form, Heading, Box, Section, Notification, Columns, Icon } from 'react-bulma-components'; //eslint-disable-line
 const { Column } = Columns;
+const { Control, Field, Select } = Form;
 
 const commonStyles = {
     width: '85%', 
@@ -43,6 +44,32 @@ export default function Profile() {
                                             `${email}`
                                         }
                                     </p>
+
+                                    <Heading size={4} textColor='light' style={{marginBottom: 10, marginLeft: '5%'}}>
+                                        Account Type
+                                    </Heading>
+                                    <Notification backgroundColor='dark' paddingless style={{marginLeft: '5%'}}>
+                                        Here you can change your user role to that of a creator. Unlocking the creator dashboard.
+                                    </Notification>
+
+                                    <Field style={{marginLeft: '5%'}} backgroundColor='dark'>
+                                        <Control backgroundColor='dark'>
+                                            <Select value='op1' backgroundColor='grey-light' color='grey-light' status='focus'>
+                                                <option value='op1'>
+                                                    User
+                                                </option>
+                                                <option value='op2'>
+                                                    Creator
+                                                </option>
+                                                <option value='op3'>
+                                                    Moderator
+                                                </option>
+                                                <option value='op4'>
+                                                    Admin
+                                                </option>
+                                            </Select>
+                                        </Control>
+                                    </Field>
                                 </Block>
                             </Box>
                         </Box>
