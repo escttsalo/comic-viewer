915b02376277672711698f782e42c3fbdec4eb7f
escttsalo <escttsalo@gmail.com>
2021-11-07 01:51:06 -0700

    some progress

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Block, Box, Container, Section, Notification } from 'react-bulma-components';
 
 export default function Profile() {
     return (
         <div>
-        HI!
+            <Section breakpoint='fluid' backgroundColor='danger'>
+                <Block breakpoint='mobile' backgroundColor='warning' color='link'>
+                    <Box style={{width: '90%', margin: 'auto'}} backgroundColor='link' color='link'>  
+                        <Notification backgroundColor='dark' color='black'>
+                            Hi Welcome to the user dashboard!
+                        </Notification>
+                    </Box>
+                </Block>
+            </Section>
         </div>
     );
 };
