de574e1906a974277f54b5c64a3f1fd46e4ff4ed
escttsalo <escttsalo@gmail.com>
2021-11-07 04:08:09 -0700

    --allow-empty

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
 import './index.css';
 import 'bulma/css/bulma.min.css';
 
 //components
 import Navigation from './components/Navigation';
 import Home from './components/Home';
+<<<<<<< HEAD
 import Footer from './components/Footer';
+=======
+import Profile from './components/Profile';
+>>>>>>> 9eeb5c8 (start routes, routed current components, start new profile component)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -22,8 +30,15 @@ function App() {
       <header className="App-header">
         <Navigation />
       </header>
-      
-      <Home />
+
+      <Switch>
+        <Route path='/profile' component={Profile}/>
+        <Route path='/home' component={Home}/>
+
+        <Route path='/'>
+          <Redirect to='/home'/>
+        </Route>
+      </Switch>
 
       <Footer />
     </div>
