a4b34a85d29783a178a3a65c46c653578ea2ceec
escttsalo <escttsalo@gmail.com>
2021-11-09 04:54:56 -0700

    add favourites box

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,7 +8,9 @@ import { Block,
     Notification, 
     Columns, 
     Icon, 
-    Tabs 
+    Image,
+    Tabs,
+    Tile 
 } from 'react-bulma-components';
 const { Column } = Columns;
 const { Checkbox, Control, Field, Select } = Form;
@@ -180,6 +182,40 @@ export default function Profile() {
                                     Favourites
                                 </Heading>
                             </Notification>
+                            <Box backgroundColor='dark'>
+                                <Tile kind='ancestor'>
+                                    <Tile>
+                                        <Tile kind='parent' vertical>
+                                            <Tile kind='child'>
+                                                <Heading subtitle textColor='light' marginless align='center'>Comic Name</Heading>
+                                                <Notification  paddingless backgroundColor='dark' textColor='light' align='center'> Comic Tags</Notification>
+                                                {/* <Heading subtitle textColor='light'>Comic rating</Heading> */}
+                                                <Image
+                                                    src='https://i.ibb.co/c8B8VXr/klemen-vrankar-m-YL3-OWayf-Uc-unsplash-1.jpg'
+                                                />
+                                            </Tile>
+                                        </Tile>
+                                    </Tile>
+                                    <Tile>
+                                        <Tile kind='parent' vertical>
+                                            <Tile kind='child' align='center'>
+                                                <Heading subtitle textColor='light' marginless>Summary</Heading>
+                                            </Tile>
+                                            <Tile kind='child'>
+                                                <Notification align='center' paddingless backgroundColor='dark' textColor='light'>
+                                                    Summary
+                                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis suscipit eaque?
+                                                </Notification>
+                                            </Tile>'
+                                            <Tile kind='child'>
+                                                <Notification paddingless align='center' backgroundColor='dark' textColor='light'>
+                                                    Rating
+                                                </Notification>
+                                            </Tile>
+                                        </Tile>
+                                    </Tile>
+                                </Tile>
+                            </Box>
                         </Box>
                     </Column>
                 </Columns>
